16f29f7fa30feb248c3ad76f27e9ffbd37419792
Nikita300704 <nikkiaugust2004@gmail.com>
2026-03-06 06:35:08 +0000

    added comaprisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,6 +23,44 @@ let someNumber=33
 let stringNumber=String(someNumber);
 console.log(stringNumber);//33 print 
 console.log(typeof stringNumber);
+//*******************OPERATIONS ************ ***//
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%2);
+
+let str1="hello";
+let str2="hitesh";
+let str3=str1+str2
+console.log(str3)
+
+//problem 
+console.log("1"+2);//if string at first all are treated as string
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");//o/p 32 if string at last first all conversion done then string 
+
+console.log(true);
+console.log(+true); //op 1
+//console.log(true+)//error
+console.log(+"");//op 0
+
+let num1,num2,num3;
+num1=num2=num3=2+2;//tricky bhevaiour should not be used 
+
+//prefix and postfix operator 
+//x++,++x
+
+
+
+
+
 
 
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+//console.log("2">1)//op true becs 2 automatically conerted to number 
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+console.log(undefined>0);//false
+console.log(undefined==0);//false
+console.log(undefined>=0);//false
+
+//comaprision like>,<,<=,>= work diffrently wrt ==
+
+//strict check ==== it also check datatypes 
+
+console.log("2"===2);//false because datatype not same 
+
+
+
+
