49922691341847dda691bf9d1cadae985db72f66
Nikita300704 <nikkiaugust2004@gmail.com>
2026-03-06 05:57:20 +0000

    added conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score="33abc";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)//output is Nan
+//Nan is a special type 
+//converison into number 
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true,0 false
+//""=>false;
+//"hitesh"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);//33 print 
+console.log(typeof stringNumber);
+
+
+
+
